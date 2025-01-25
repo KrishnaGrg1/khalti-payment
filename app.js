f94722407dd11baab5ab46598e26e01d04729bd9
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-01-25 10:27:54 +0545

    added the frontend for purchasing the plan

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json());
 
 connectToMongoDB().then((connectMessage)=>{
   console.log(connectMessage);
+  app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
   app.use(mainRouter)
   app.listen(port,()=>{
     console.log("Server is listening on Port : "+port)
--- routes/payment.js
+++ routes/payment.js
@@ -2,11 +2,18 @@ import { Router } from "express";
 import paymentController from "../controllers/payment.js";
 import validate from "../middlewares/valdation.js";
 import validationSchema from "../validations/payment.js"
+import SubscriptionPlan from "../models/subscriptionPlan.js";
 
 
 const paymentRouter=Router()
 
-
+paymentRouter.get('/add',async(req,res)=>{
+    const subscriptionPlans =await SubscriptionPlan.find({});
+    res.render('purchase',{
+        subscriptionPlans,
+        message:''
+    })
+})
 
 paymentRouter.post('/add',validate(validationSchema.addpayment),paymentController.initiatePayment);
 
